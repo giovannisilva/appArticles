2037c07edbe8c84abf704261a112fc0e45bc9395
Noturno <giovanni.dgs@hotmail.com>
2017-02-16 20:33:11 -0200

    criacao tela de cadastro

---
+++ AppList/CreateAccountDataProvider.swift
@@ -0,0 +1,9 @@
+//
+//  CreateAccountDataProvider.swift
+//  AppList
+//
+//  Created by Noturno on 16/02/17.
+//  Copyright © 2017 Impacta. All rights reserved.
+//
+
+import Foundation
---
+++ AppList/CreateAccountViewController.swift
@@ -0,0 +1,46 @@
+//
+//  CreateAccountViewController.swift
+//  AppList
+//
+//  Created by Noturno on 16/02/17.
+//  Copyright © 2017 Impacta. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func voltarButton(_ sender: UIButton) {
+        
+    }
+    
+    
+    
+    
+    
+    
+}
